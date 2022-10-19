ad69e76a953fff69f7a18c5b85d9d0f11af36e9a
bthirion <bertrand.thirion@inria.fr>
2022-10-19 12:24:16 +0200

    attempte to add a solver for standard regression

--- mtw/solver.py
+++ mtw/solver.py
@@ -73,6 +73,7 @@ def solver(X, Y, M, alpha=1., beta=0., epsilon=0.01, gamma=1., sigma0=0.,
         sigma0 *= np.linalg.norm(Y, axis=1).min() / (n_samples ** 0.5)
     sigmas = np.ones(n_tasks)
     R, b1, b2 = None, None, None
+    stop
     with Parallel(n_jobs=n_jobs, backend="threading") as pll:
         if alpha == 0.:
             t = time()
@@ -103,6 +104,7 @@ def solver(X, Y, M, alpha=1., beta=0., epsilon=0.01, gamma=1., sigma0=0.,
                 Y1 = utils.residual(X, - theta2, Y)
             else:
                 Y1 = Y
+            print(marginals1)
             theta1, R, sigmas = update_coefs(pll, X, Y1, Ls, marginals1,
                                              sigmas,
                                              a, b,
@@ -259,3 +261,205 @@ def update_coefs(pll, X, y, Ls, marginals, sigmas, a, b, sigma0,
 
     sigmas = np.r_[sigmas]
     return thetas, R, sigmas
+
+
+def _update_coefs(pll, X, y, Ls, marginals, sigmas, a, b, sigma0,
+                 coefs0=None, R=None, maxiter=20000, tol=1e-4,
+                 positive=False):
+    """BCD in numba."""
+    n_tasks, n_samples, n_features = Y.shape
+    n_samples, n_components = X.shape
+    n_features = Ls.shape[1] ###
+
+    if coefs0 is None:
+        R = y.copy()
+        coefs0 = np.zeros((n_features, n_components, n_tasks))
+    elif R is None:
+        R = utils.residual(X, coefs0, y)
+
+    dell = delayed(cython_wrapper)
+    it = (dell(n_samples, n_features, np.asfortranarray(X[k].copy()), y[k],
+               Ls[k], marginals[k], coefs0[:, k], R[k].copy(),
+               sigmas[k:k + 1], a, b, sigma0, tol, maxiter, positive)
+          for k in range(n_tasks))
+    output = pll(it)
+
+    thetas, R, sigmas = list(zip(*output))
+    thetas = np.stack(thetas, axis=1)
+    R = np.stack(R, axis=0)
+
+    sigmas = np.r_[sigmas]
+    return thetas, R, sigmas
+
+
+def _solver(X, Y, M, alpha=1., beta=0., epsilon=0.01, gamma=1., sigma0=0.,
+           stable=False, maxiter=2000, callback=None, tol=1e-5,
+           maxiter_ot=10000, tol_ot=1e-4, positive=False, n_jobs=1,
+           tol_cd=1e-4, gpu=True, verbosxe=True):
+    """Perform Alternating Optimization of concomittant CRW."""
+
+    log = {'loss': [], 'dloss': [], 'log_sinkhorn1': [], 'log_sinkhorn2': [],
+           'stable': stable, 't_cd': 0., 't_ot': 0., "times": [], "objcd": [],
+           "fot1": [], "fot2": []}
+
+    n_tasks, n_samples, n_features = Y.shape
+    n_samples, n_components = X.shape
+    
+    coefs01 = np.ones((n_features, n_components, n_tasks)) ###
+    coefs02 = np.ones((n_features, n_components, n_tasks)) ###
+    marginals1 = np.ones((n_tasks, n_components, n_features)) / n_features
+    marginals2 = np.ones((n_tasks, n_components, n_features)) / n_features
+
+    Xf = [np.asfortranarray(np.eye(n_features)) for k in range(n_tasks)]
+    mXf = [- np.asfortranarray(np.eye(n_features)) for k in range(n_tasks)]
+
+    theta1 = coefs01.copy()
+    theta2 = coefs02.copy()
+    theta = theta1 - theta2
+
+    thetaold = theta.copy()
+    Ls = (X ** 2).sum(axis=1) ###
+    Ls[Ls == 0.] = Ls[Ls != 0].min()
+
+    ot_img = True
+    if len(M) == n_features:
+        ot_img = False
+    # if ot_img, fast convolutions will be used in Sinkhorn
+
+    update_ot_1 = set_ot_func(stable, ot_img)
+    update_ot_2 = set_ot_func(stable, ot_img)
+
+    t_cd = []
+    t_ot = 0.
+    xp = utils.set_module(gpu)
+    M = xp.asarray(- M / epsilon)
+
+    thetabar1 = np.ones_like(coefs01).mean(axis=-1)
+    thetabar2 = np.ones_like(coefs02).mean(axis=-1)
+    theta2 *= 0.
+    thetabar2 *= 0.
+    theta = theta1.copy()
+
+    a = n_samples * alpha * gamma
+    b = n_samples * beta
+    if alpha == 0.:
+        theta1 *= 0.
+        theta2 *= 0.
+    if sigma0:
+        sigma0 *= np.linalg.norm(Y, axis=1).min() / (n_samples ** 0.5)
+    sigmas = np.ones(n_tasks)
+    R, b1, b2 = None, None, None
+
+    # prepare the solution
+    Y_ = np.array([np.linalg.pinv(X[k]).dot(Y[k]) for k in range(n_tasks)])
+
+    Theta1, Theta2, Thetabar1, Thetabar2, Log, Sigmas = [] * 6
+    for comp in range(n_components):
+        with Parallel(n_jobs=n_jobs, backend="threading") as pll:
+            if alpha == 0.: ### no Wasserstein
+                t = time()
+                theta1 = np.asfortranarray(theta1)
+                theta, R, sigmas = _update_coefs(pll, Xf, Y_[comp], Ls, marginals1,
+                                                 sigmas,
+                                                 a, b,
+                                                 sigma0,
+                                                 coefs0=theta1,
+                                                 R=R,
+                                                 tol=tol_cd, maxiter=20000,
+                                                 positive=positive)
+                obj = 0.5 * (R ** 2).sum(axis=1).dot(1 / sigmas) / n_samples
+                obj += beta * abs(theta).sum() + 0.5 * sigmas.sum()
+                theta1, theta2 = utils.get_unsigned(theta)
+                t_cd.append(time() - t)
+                log["t_cd"] = sum(t_cd)
+                log['loss'].append(obj)
+                log['dloss'].append(0)
+
+                if callback:
+                    callback(theta, v=obj)
+
+                return theta1, theta2, thetabar1, thetabar2, log, sigmas
+            for i in range(maxiter):
+                t0 = time()
+                theta1, R, sigmas = _update_coefs(pll, Xf, Y_[comp], Ls, marginals1,
+                                                 sigmas,
+                                                 a, b,
+                                                 sigma0,
+                                                 coefs0=theta1,
+                                                 R=R,
+                                                 tol=tol_cd, maxiter=10000)
+                theta = theta1.copy()
+
+                obj = 0.5 * (R ** 2).sum(axis=1).dot(1 / sigmas) / n_samples
+                obj += beta * (theta1 + theta2).sum() + 0.5 * sigmas.sum()
+                log["objcd"].append(obj)
+                tc = time() - t0
+                t_cd.append(tc)
+                dx = abs(theta - thetaold).max() / max(1, thetaold.max(),
+                                                       theta.max())
+
+                thetaold = theta.copy()
+
+                t1 = time()
+                if alpha:
+                    if (theta1 > 1e-10).any(0).all():
+                        fot1, log_ot1, marginals1, b1, q1 = \
+                            update_ot_1(theta1, M, epsilon, gamma,
+                                        b=b1, tol=tol_ot, maxiter=maxiter_ot)
+                        if fot1 is None or not theta1.max(0).all():
+                            warnings.warn("""Nan found when computing barycenter,
+                                             re-fit in log-domain.""")
+                            b1 = xp.log(b1 + 1e-100, out=b1)
+                            stable = True
+                            update_ot_1 = set_ot_func(True, ot_img)
+                            fot1, log_ot1, marginals1, b1, q1 = \
+                                update_ot_1(theta1, M, epsilon, gamma, b=b1,
+                                            tol=tol_ot, maxiter=maxiter_ot)
+                            utils.free_gpu_memory(xp)
+
+                        log["log_sinkhorn1"].append(log_ot1)
+                        thetabar1 = q1
+                        log["fot1"].append(fot1)
+                        obj += alpha * fot1
+
+                t_ot += time() - t1
+                if callback:
+                    callback(theta, v=obj)
+
+                log['loss'].append(obj)
+                log['dloss'].append(dx)
+                log['times'].append(time() - t0)
+
+                if dx < tol:
+                    break
+        if i == maxiter - 1:
+            print("\n"
+                  "******** WARNING: Stopped early in main loop. *****\n"
+                  "\n"
+                  "You may want to increase mtw.maxiter.")
+
+        if verbose:
+            print("Time ot %.1f | Time cd %.1f" % (t_ot, sum(t_cd)))
+
+        log['stable'] = stable
+        log['t_cd'] = sum(t_cd)
+        log['t_ot'] = t_ot
+
+        theta2 *= 0.
+        thetabar2 = xp.zeros_like(thetabar1)
+        try:
+            thetabar2 = thetabar2.get()
+        except AttributeError:
+            pass
+        Theta1.append(theta1)
+        Theta2.append(theta2)
+        Thetabar1.append(thetabar1)
+        Thetabar2.append(thetabar2)
+        Log.append(log)
+        Sigmas.append(sigmas)
+        
+    Theta1 = np.array(Theta1)
+    Theta2 = np.array(Theta2)
+    Thetabar1 = np.array(Thetabar1)
+    Thetabar2 = np.array(Thetabar2)
+    return Theta1, Theta2, Thetabar1, Thetabar2, Log, Sigmas
